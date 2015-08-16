c9ed8939bca54e056178948385f950f266c95a2d
김병곤 <fharenheit@gmail.com>
2015-08-16 21:02:45 +0900

    일단 에러 없음.

--- src/main/java/org/opencloudengine/users/fharenheit/aspectj/KafkaAspect.java
+++ src/main/java/org/opencloudengine/users/fharenheit/aspectj/KafkaAspect.java
@@ -1,12 +1,27 @@
 package org.opencloudengine.users.fharenheit.aspectj;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import kafka.server.KafkaConfig;
+import kafka.utils.SystemTime;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class KafkaAspect {
 
+    @Pointcut(value = "execution (* kafka.server.KafkaServer.*(..)) && args(config, time)", argNames = "joinPoint,config,time")
+    public void kafkaServerConstruct(JoinPoint joinPoint, KafkaConfig config, SystemTime time) {
+
+    }
+
+    @After(value = "kafkaServerConstruct(joinPoint, config, time)", argNames = "joinPoint,config,time")
+    public void message(JoinPoint joinPoint, KafkaConfig config, SystemTime time) {
+        System.out.println("---------------------------------------");
+        System.out.println(config.getClass().getName());
+    }
+
+/*
     @Around("execution(* kafka.server.KafkaServer.new(...)")
     public Object interceptAndLog(ProceedingJoinPoint invocation) throws Throwable {
         try {
@@ -16,5 +31,6 @@ public class KafkaAspect {
             throw e;
         }
     }
+*/
 
 }
