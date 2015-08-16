d50a0d3faff3e3b0ca8b87b9129ef8d83afa73ac
김병곤 <fharenheit@gmail.com>
2015-08-17 00:09:28 +0900

    Byteman 추가.

--- src/main/java/org/opencloudengine/users/fharenheit/bm/KafkaAgent.java
+++ src/main/java/org/opencloudengine/users/fharenheit/bm/KafkaAgent.java
@@ -1,13 +1,41 @@
 package org.opencloudengine.users.fharenheit.bm;
 
 import kafka.server.KafkaServer;
+import org.opencloudengine.users.fharenheit.SystemUtils;
+
+import javax.management.*;
+import java.lang.management.ManagementFactory;
+import java.util.Iterator;
+import java.util.Set;
 
 public class KafkaAgent {
 
-    public static void start(KafkaServer server) {
+    public static void start(KafkaServer server) throws IntrospectionException, InstanceNotFoundException, ReflectionException, AttributeNotFoundException, MBeanException {
         System.out.println("-----------------------------");
-        System.out.println("Server State : " + server.config().hostName());
+        System.out.println("Kafka Server : " + server.loggerName());
         System.out.println("-----------------------------");
-    }
 
+        String pid = SystemUtils.getPid();
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        Set<ObjectInstance> mbeans = mbs.queryMBeans(null, null);
+
+        Iterator<ObjectInstance> iterator = mbeans.iterator();
+
+        while (iterator.hasNext()) {
+            ObjectInstance mbean = iterator.next();
+            System.out.printf("==============================================");
+            System.out.println("Class Name:\t" + mbean.getClassName());
+            System.out.println("Object Name:\t" + mbean.getObjectName());
+
+            final MBeanAttributeInfo[] attributes = mbs.getMBeanInfo(mbean.getObjectName()).getAttributes();
+            for (final MBeanAttributeInfo attribute : attributes) {
+                Object value = mbs.getAttribute(mbean.getObjectName(), attribute.getName());
+                System.out.printf("Name : " + attribute.getName());
+                System.out.printf("Value : " + value);
+                System.out.printf("----------------------------------------");
+            }
+
+        }
+
+    }
 }
